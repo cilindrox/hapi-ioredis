6685077f5cca74aadddbed187333073b6ec5d047
Gaston Festari <cilindrox@gmail.com>
2015-11-20 11:40:35 -0300

    Parse options.url

--- test/index.js
+++ test/index.js
@@ -31,3 +31,21 @@ it('can be added as a plugin to Hapi', (done) => {
         done();
     });
 });
+
+
+it('parses a given redis URL', (done) => {
+
+    const server = new Hapi.Server();
+    const register = {
+        register: ioRedis,
+        options: { url: 'redis://:@127.0.0.1:6379/' }
+    };
+
+    server.register(register, (err) => {
+
+        expect(err).to.not.exist();
+        expect(server.plugins['hapi-ioredis']).to.exist();
+        expect(server.plugins['hapi-ioredis'].client).to.be.an.object();
+        done();
+    });
+});
