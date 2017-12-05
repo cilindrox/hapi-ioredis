5e50c3f16ec0a11af462c75c038042f4beb0b5ec
James Dixon <jim.w.dixon@gmail.com>
2017-12-04 23:17:12 -0700

    add hapi v17 compatibility

--- test/index.js
+++ test/index.js
@@ -11,7 +11,6 @@ const IoRedis = require('../');
 
 const internals = {};
 
-
 // Test shortcuts
 
 const lab = exports.lab = Lab.script();
@@ -19,100 +18,87 @@ const it = lab.it;
 const expect = Code.expect;
 
 
-it('can be added as a plugin to Hapi', (done) => {
+it('can be added as a plugin to Hapi', async () => {
 
     const server = new Hapi.Server();
     const plugin = {
-        register: IoRedis,
+        plugin: IoRedis,
         options: { url: 'redis://:@127.0.0.1:6379/' }
     };
 
-    server.register(plugin, (err) => {
+    await server.register(plugin);
 
-        expect(err).to.not.exist();
-        expect(server.app.redis).to.exist();
-        expect(server.app.redis.disconnect).to.be.a.function();
-        done();
-    });
+    expect(server.app.redis).to.exist();
+    expect(server.app.redis.disconnect).to.be.a.function();
 });
 
 
-it('can be registered with a redis options config object', (done) => {
+it('can be registered with a redis options config object', async () => {
 
     const server = new Hapi.Server();
     const plugin = {
-        register: IoRedis,
+        plugin: IoRedis,
         options: {
             redis: { host: '127.0.0.1', port: '6379' }
         }
     };
 
-    server.register(plugin, (err) => {
+    await server.register(plugin);
 
-        expect(err).to.not.exist();
-        expect(server.app.redis).to.exist();
-        expect(server.app.redis.disconnect).to.be.a.function();
-        done();
-    });
+    expect(server.app.redis).to.exist();
+    expect(server.app.redis.disconnect).to.be.a.function();
 });
 
 
-it('decorates the request object with a redis prop', (done) => {
+it('decorates the request object with a redis prop', async () => {
 
     const server = new Hapi.Server();
     const plugin = {
-        register: IoRedis,
+        plugin: IoRedis,
         options: { url: 'redis://:@127.0.0.1:6379/' }
     };
 
-    server.register(plugin, (err) => {
+    await server.register(plugin);
 
-        expect(err).to.not.exist();
+    server.route({
+        method: 'GET',
+        path: '/',
+        config: {
+            handler: (request, h) => {
 
-        server.connection();
-        server.route({
-            method: 'GET',
-            path: '/',
-            config: {
-                handler: (request, reply) => {
-
-                    expect(request.redis).to.exist();
-                    expect(request.redis.disconnect).to.be.a.function();
-                    return reply('GREAT SUCCESS!');
-                }
+                expect(request.redis).to.exist();
+                expect(request.redis.disconnect).to.be.a.function();
+                return 'GREAT SUCCESS!';
             }
-        });
-
-        const payload = {
-            method: 'GET',
-            url: '/'
-        };
-
-        server.inject(payload, (response) => {
-
-            expect(response.statusCode).to.equal(200);
-            expect(response.result).to.equal('GREAT SUCCESS!');
-            done();
-        });
+        }
     });
+
+    await server.start();
+
+    const payload = {
+        method: 'GET',
+        url: '/'
+    };
+
+    const response = await server.inject(payload);
+
+    expect(response.statusCode).to.equal(200);
+    expect(response.result).to.equal('GREAT SUCCESS!');
 });
 
-it('can override the name of the redis instance on server.app', (done) => {
+it('can override the name of the redis instance on server.app', async () => {
 
     const server = new Hapi.Server();
     const plugin = {
-        register: IoRedis,
+        plugin: IoRedis,
         options: {
             redis: { host: '127.0.0.1', port: '6379' },
             name: 'myRedisInstance'
         }
     };
 
-    server.register(plugin, (err) => {
+    await server.register(plugin);
 
-        expect(err).to.not.exist();
-        expect(server.app.myRedisInstance).to.exist();
-        expect(server.app.myRedisInstance.disconnect).to.be.a.function();
-        done();
-    });
+    expect(server.app.myRedisInstance).to.exist();
+    expect(server.app.myRedisInstance.disconnect).to.be.a.function();
 });
